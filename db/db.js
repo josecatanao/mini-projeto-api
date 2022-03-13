09127a4b8474128c3e7490917b4d4295722eb9f8
josecatanao <51245690+josecatanao@users.noreply.github.com>
2022-03-13 10:37:37 -0300

    adicionando conexão

--- db/db.js
+++ db/db.js
@@ -1,8 +1,13 @@
 const Sequelize = require('sequelize');
-const sequelize = new Sequelize('users','postgres', '0000',{
-    dialect:'postgres',
-    host:'localhost'
-})
+sequelize = new Sequelize("postgres://nzmgklgxvfzrkk:c15a9bc2c4a618b0c8011a4e320f4b8924880f97d6041a0c98d95996870eba51@ec2-34-224-226-38.compute-1.amazonaws.com:5432/d46ssmmc81sv6f", {
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
+      }
+    }
+  }
+);
 
 
 module.exports = sequelize;
